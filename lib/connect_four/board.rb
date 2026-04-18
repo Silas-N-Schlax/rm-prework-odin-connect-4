02e8fd501acee16b9c8bee569d88eebd6ee6660f
SilasNS <silasschlax@gmail.com>
2026-04-18 19:25:57 -0400

    Create Board class with tests
    Board class manages the board from sending, updating, and checking
    win conditions.

---
+++ lib/connect_four/board.rb
@@ -0,0 +1,86 @@
+# Board class
+class Board
+  def initialize(board = Array.new(7) { [] })
+    @board = board
+  end
+
+  def update_board(column, peice)
+    return false if full?(column)
+
+    @board[column].push(peice)
+    true
+  end
+
+  def full?(column)
+    true if @board[column].length >= 6
+  end
+
+  def full_board?
+    full_columns = 0
+    @board.length.times do |i|
+      full_columns += 1 if full?(i)
+    end
+    return true if full_columns >= @board.length
+
+    false
+  end
+
+  def game_over?(column)
+    true if winner?(column) && full_board?
+  end
+
+  def winner?(column)
+    directions = [[1, 0], [0, 1], [1, 1], [-1, 1]] # [column, row]
+    directions.each do |dir|
+      row = @board[column].length - 1
+      result = check_direction(dir, column, row, @board[column][row])
+      return true if result
+    end
+    false
+  end
+
+  def check_direction(dir, column, row, peice)
+    test_case = [
+      get_player_peices_in_direction(column, row, dir.map { |x| x * -1 }, peice, []),
+      @board[column][row],
+      get_player_peices_in_direction(column, row, dir, peice, [])
+    ].join(" ").split(" ")
+    true if test_case.length >= 4
+  end
+
+  def get_player_peices_in_direction(column, row, dir, peice, list)
+    return list if @board[column + dir[0]].nil? || @board[column + dir[0]][row + dir[1]].nil? || row + dir[1] == -1
+
+    space = @board[column + dir[0]][row + dir[1]]
+    return list if space != peice
+
+    list << space
+    get_player_peices_in_direction(column + dir[0], row + dir[1], dir, peice, list)
+  end
+
+  def send_board
+    board = generate_board
+    board.each { |line| puts line }
+  end
+
+  def generate_board
+    constant_board = ["╔═══╦═══╦═══╦═══╦═══╦═══╦═══╗", "  1   2   3   4   5   6   7", "╚═══╩═══╩═══╩═══╩═══╩═══╩═══╝"]
+    middle_board = generate_middle_board
+    constant_board.each { |const| middle_board.unshift("\e[36m#{const}\e[0m") }
+    middle_board.rotate(1)
+  end
+
+  def generate_middle_board
+    board = Array.new(6) { [] }
+    @board.each_with_index do |column, i|
+      column = column.reverse
+      6.times do |j|
+        board[j].push(i.zero? ? "\e[36m║ #{column[j] || ' '} \e[36m║" : " #{column[j] || ' '} \e[36m║")
+      end
+    end
+    board.each_with_index do |item, i|
+      board[i] = item.join
+    end
+    board.reverse!
+  end
+end
---
+++ spec/connect_four/board_spec.rb
@@ -0,0 +1,84 @@
+require_relative "../../lib/connect_four/board"
+
+describe Board do
+  describe "#update_board" do
+    context "when given row that is not full" do
+      subject(:board) { described_class.new }
+      it "returns true" do
+        expect(board.update_board(1, "x")).to be(true)
+      end
+      it "updates row" do
+        board.update_board(1, "x")
+        new_board = board.instance_variable_get(:@board)
+        expect(new_board[1][0]).to eq("x")
+      end
+    end
+    context "when given a row that is full" do
+      subject(:full_board) { described_class.new(Array.new(7) { Array.new(6) }) }
+      it "returns false" do
+        expect(full_board.update_board(1, "x")).to be(false)
+      end
+      it "does not update row" do
+        full_board.update_board(1, "x")
+        new_board = full_board.instance_variable_get(:@board)
+        expect(new_board[1].length).to be(6)
+      end
+    end
+  end
+
+  describe "#winner?" do
+    context "when player places the winning peice vertical" do
+      subject(:winning_game) { described_class.new([%w[x x x x], [], [], [], [], [], []]) }
+      it "returns true" do
+        expect(winning_game.winner?(0)).to be(true)
+      end
+    end
+    context "when player places the winning peice horizontal" do
+      subject(:winning_game) { described_class.new([["x"], ["x"], ["x"], ["x"], [], [], []]) }
+      it "returns true" do
+        expect(winning_game.winner?(0)).to be(true)
+      end
+    end
+    context "when player places the winning peice diagonal" do
+      subject(:winning_game) { described_class.new([["x"], [nil, "x"], [nil, nil, "x"], [nil, nil, nil, "x"], [], [], []]) }
+      it "returns true" do
+        expect(winning_game.winner?(0)).to be(true)
+      end
+    end
+    context "when player places the winning peice anti-diagonal" do
+      subject(:winning_game) { described_class.new([[nil, nil, nil, "x"], [nil, nil, "x"], [nil, "x"], ["x"], [], [], []]) }
+      it "returns true" do
+        expect(winning_game.winner?(0)).to be(true)
+      end
+    end
+    context "when player does not place the winning peice" do
+      subject(:going_game) { described_class.new([%w[x x x y], [], [], [], [], [], []]) }
+      it "return false" do
+        expect(going_game.winner?(0)).to be(false)
+      end
+    end
+  end
+  describe "#game_over?" do
+    context "when player places winning peice and fills board" do
+      subject(:winning_game) { described_class.new(Array.new(7) { Array.new(6, "x") }) }
+      it "returns true" do
+        expect(winning_game.game_over?(0)).to be(true)
+      end
+    end
+    context "when player does not place a winning peice and fills the board" do
+      array = [
+        ["x", nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil]
+      ]
+      subject(:winning_game) { described_class.new(array) }
+      it "returns nil" do
+        expect(winning_game.game_over?(0)).to be(nil)
+      end
+    end
+  end
+end
